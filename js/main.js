84e5d816725a47e92504561ccff2755c46a66041
Alexander Mendez <bluespace@gmail.com>
2016-08-20 17:39:42 -0700

    update padding

--- js/main.js
+++ js/main.js
@@ -14,31 +14,7 @@
 	});
     var h = $(window).height();
     var w = $(window).width();
-    // $('#intro').css('top', 50+'px');
-    // $('#intro').css('height', h-50+'px');
-    // $('.image-wrapper img').css('height', h-100+'px');
-    
 
-	// $('ul.nav li.dropdown').hover(function() {
-	//   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-	// }, function() {
-	//   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
-	// });
-
-	// jQuery to collapse the navbar on scroll
-	// $(window).scroll(function() {
-		// $('navbar-collapse').toggleClass('in');
-		// if ($(".navbar").offset().top > 0) {
-		// 	$(".navbar-fixed-top").addClass("top-nav-collapse");
-		// } else {
-		// 	$(".navbar-fixed-top").removeClass("top-nav-collapse");
-		// }
-		// if ($(this).scrollTop() >= h) {
-		// 	$('.navbar-brand').html("<a href='#intro'>Into The Distant Sky</a>");
-		// } else {
-		// 	$('.navbar-brand').html('<i class="fa fa-chevron-down"></i> Scroll to learn more');
-		// };
-	// });
 	
 	// smooth links
 	var $root = $('html, body');
@@ -73,16 +49,6 @@
     
     
     
-	// $('.conebox').nivoLightbox({
-	//     effect: 'fadeScale',
-	//     theme: 'default',
-	//     keyboardNav: true,
-	//     clickOverlayToClose: true,
-	//     errorMessage: 'The requested content cannot be loaded. Please try again later.',
-	//     });
-    
-    
-    
     var owl = $('.owl-carousel');
     if (w < 800) {
         var navtext = ['<i class="fa fa-2x fa-chevron-left"></i><br />Closer to us', '<i class="fa fa-2x fa-chevron-right"></i><br>Further away']
@@ -112,68 +78,15 @@
             owl.trigger('next.owl');
         }
     });
-    // owl.on('mousewheel', '.owl-bottom', function (e) {
-    //     if (e.deltaY<0) {
-    //         owl.trigger('next.owl');
-    //     } else {
-    //         owl.trigger('prev.owl');
-    //     }
-    //     // e.preventDefault();
-    // });
-    
-    // $('#navicon').click(function (ev) {
-    //     $('#navicon').toggleClass('fa-chevron-down');
-    //     // $('#explore-subtitle').toggleClass('link-hide')
-    // });
-    //
-    //
-    //
-    // $('#explore-open').click(function (ev) {
-    //     $('#explore').toggleClass('link-hide');
-    //     // $('#explore-subtitle').toggleClass('link-hide')
-    // });
-    //
-    // $('#use-open').click(function (ev) {
-    //     $('#use').toggleClass('link-hide');
-    //     // $('#use-subtitle').toggleClass('link-hide')
-    // });
-    //
-    // $('#involve-open').click(function (ev) {
-    //     $('#involve').toggleClass('link-hide');
-    //     // $('#involve-subtitle').toggleClass('link-hide')
-    // });
-    
-    
     
     $('.infoexpand').hide();
     $('.infoheader h3').click(function() {
         $(this).parent().find('ul').slideToggle();
     });
-	
-    // var clickflag = false;
 	$('img.hovercone').bind("click touchstart", function (e) {
 	    'use strict'; //satisfy code inspectors
-        // if (!clickflag) {
-        //     clickflag=true;
-        //     setTimeout(function(){clickflag=false;}, 100);
-        // };
 	    var link = $(this); //preselect the link
 		link.toggleClass('hover');
-		// link.fadeToggle('slow');
-		
-		
-			// 	    if (link.hasClass('hover')) {
-			// // link.fadeOut();
-			//             link.removeClass('hover');
-			//
-			// 	        return true;
-			// 	    } else {
-			// 	        link.addClass('hover');
-			// // link.fadeIn();
-			//
-			// 	        e.preventDefault();
-			// 	        return false; //extra, and to make sure the function has consistent return points
-			// 	    }
 	});
 	
     
